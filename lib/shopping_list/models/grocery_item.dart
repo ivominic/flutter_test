349807331601964334cdf09645778df56f5fe889
ivominic <ivo.minic@gmail.com>
2024-05-05 18:41:15 +0200

    Setting new app

---
+++ lib/shopping_list/models/grocery_item.dart
@@ -0,0 +1,15 @@
+import 'package:test_app/shopping_list/models/category.dart';
+
+class GroceryItem {
+  const GroceryItem({
+    required this.id,
+    required this.name,
+    required this.quantity,
+    required this.category,
+  });
+
+  final String id;
+  final String name;
+  final int quantity;
+  final Category category;
+}
