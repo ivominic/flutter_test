d81899c9baa32f9de477a368b6e5f1e2308d2454
ivominic <ivo.minic@gmail.com>
2024-05-28 07:03:50 +0200

    Favourite places test app - front/design

---
+++ lib/favorite_places/screens/places.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:test_app/favorite_places/screens/add_place.dart';
+import 'package:test_app/favorite_places/widgets/places_list.dart';
+
+class PlacesScreen extends StatelessWidget {
+  const PlacesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Your places"),
+        actions: [
+          IconButton(
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (ctx) => const AddPlaceScreen(),
+                ),
+              );
+            },
+            icon: const Icon(Icons.add),
+          ),
+        ],
+      ),
+      body: PlacesList(places: []),
+    );
+  }
+}
