5c3279544a3b0d92bce18fcf167b75bc6b21a287
ivominic <ivo.minic@gmail.com>
2024-05-28 07:40:36 +0200

    Places detail page added

--- lib/favorite_places/widgets/places_list.dart
+++ lib/favorite_places/widgets/places_list.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:test_app/favorite_places/models/place.dart';
+import 'package:test_app/favorite_places/screens/place_detail.dart';
 
 class PlacesList extends StatelessWidget {
   const PlacesList({super.key, required this.places});
@@ -30,6 +31,13 @@ class PlacesList extends StatelessWidget {
               .titleMedium!
               .copyWith(color: Theme.of(context).colorScheme.onBackground),
         ),
+        onTap: () {
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (ctx) => PlaceDetailScreen(place: places[index]),
+            ),
+          );
+        },
       ),
     );
   }
